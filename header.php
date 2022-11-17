9d250090fff23b6ef5b75d3a9ba462bba29fece2
Antony.Huart <antony.huart37@gmail.com>
2022-11-17 18:38:28 +1100

    cration page in

---
+++ header.php
@@ -0,0 +1,56 @@
+<div class="background">
+  <nav>
+    <a href="index.php">
+      <div class="logo">
+        <p class="logo__name logo__name--1">We love</p>
+        <img class="logo__img" src="./Img/logo.png" alt="logo">
+        <p class="logo__name logo__name--2">the World</p>
+      </div>
+    </a>
+    <div class="separation-line-menu"></div>
+    <ul class="main-menu">
+      <li class="main-menu__drop-down-menu"><a href="inProgress.php">Préparatifs  </a><span class="main-menu__drop-down-menu__nav-arrow"></span>
+        <ul class="main-menu__drop-down-menu__under-menu main-menu__drop-down-menu__under-menu--preparatif">
+          <li><a href="inProgress.php">Voyage en PVT</a></li>
+          <li><a href="inProgress.php">Sac à dos</a></li>
+        </ul>
+      </li>
+      <li class="main-menu__drop-down-menu"><a href="inProgress.php">Destinations  </a><span class="main-menu__drop-down-menu__nav-arrow"></span>
+        <ul class="main-menu__drop-down-menu__under-menu">
+          <li class="main-menu__drop-down-menu__2">Océanie
+            <ul class="main-menu__drop-down-menu__under-menu__2">
+              <li><a href="inProgress.php">Australie</a></li>                               
+              <li><a href="inProgress.php">Nouvelle-Zélande</a></li>                                 
+            </ul>
+          </li>    
+          <li class="main-menu__drop-down-menu__2">Asie
+            <ul class="main-menu__drop-down-menu__under-menu__2">
+              <li><a href="inProgress.php">Vietnam</a></li>                               
+              <li><a href="inProgress.php">Cambodge</a></li>                                 
+              <li><a href="inProgress.php">Laos</a></li>                                 
+            </ul>
+          </li>    
+          <li class="main-menu__drop-down-menu__2">Europe
+            <ul class="main-menu__drop-down-menu__under-menu__2">
+              <li><a href="inProgress.php">France</a></li>                               
+              <li><a href="inProgress.php">Espagne</a></li>                                 
+              <li><a href="inProgress.php">Portugal</a></li>                                 
+            </ul>
+          </li>    
+        </ul>
+      </li>
+      <li><a href="inProgress.php">Vanlife</a></li>
+      <li><a href="inProgress.php">Bon plans</a></li>                
+      <li class="main-menu__drop-down-menu"><a href="inProgress.php">Photos  </a><span class="main-menu__drop-down-menu__nav-arrow"></span>
+        <ul class="main-menu__drop-down-menu__under-menu main-menu__drop-down-menu__under-menu--pictures">
+          <li><a href="inProgress.php">Australie</a></li>
+          <li><a href="inProgress.php">Nouvelle-Zélande</a></li>
+          <li><a href="inProgress.php">Vietnam</a></li>
+          <li><a href="inProgress.php">Cambodge</a></li>
+          <li><a href="inProgress.php">Laos</a></li>
+        </ul>
+      </li>    
+      <li><a href="inProgress.php">À propos</a></li>
+    </ul>
+  </nav>
+</div>
---
+++ inProgress.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/style.css">
+    <script src="./js/album.js" async></script>
+    <title>En cours de création</title>
+  </head>
+  <header>
+    <?php include_once('./header.php'); ?>
+  </header>
+  <body>
+    <div class="in-progress">
+      <h1>Cette page sera bientôt disponible !</h1>
+    </div>
+    <div class="frame">
+      <div class="cog-first">
+        <div class="cog-first__part"></div>
+        <div class="cog-first__part"></div>
+        <div class="cog-first__part"></div>
+        <div class="cog-first__part"></div>
+        <div class="cog-first__part"></div>
+        <div class="cog-first__part"></div>
+        <div class="cog-first__part"></div>
+        <div class="cog-first__part"></div>
+        <div class="cog-first__hole"></div>
+        <div class="cog-first__line"></div>   
+      </div>
+      <div class="cog-second">
+        <div class="cog-second__part"></div>
+        <div class="cog-second__part"></div>
+        <div class="cog-second__part"></div>
+        <div class="cog-second__part"></div>
+        <div class="cog-second__part"></div>
+        <div class="cog-second__part"></div>
+        <div class="cog-second__part"></div>
+        <div class="cog-second__part"></div>
+        <div class="cog-second__hole"></div>
+        <div class="cog-second__line"></div>   
+      </div>
+      <div class="cog-third">
+        <div class="cog-third__part"></div>
+        <div class="cog-third__part"></div>
+        <div class="cog-third__part"></div>
+        <div class="cog-third__part"></div>
+        <div class="cog-third__part"></div>
+        <div class="cog-third__part"></div>
+        <div class="cog-third__part"></div>
+        <div class="cog-third__part"></div>
+        <div class="cog-third__hole"></div>
+        <div class="cog-third__line"></div>   
+      </div>
+      <p>In progress</p>
+      <div class="dot">
+        <div class="dot dot--1"></div>
+        <div class="dot dot--2"></div>
+        <div class="dot dot--3"></div>
+      </div>
+    </div>  
+  </body>
+  <footer>
+    <?php include_once('./footer.php'); ?>
+  </footer>
+</html>
